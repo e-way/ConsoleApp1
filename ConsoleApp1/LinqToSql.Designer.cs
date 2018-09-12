6319c7b81b01ef2c8e4a68c3e77fa0c8b79c3208
yong yang <y9415019@hotmail.com>
2018-09-11 23:37:20 -0700

    Add project files.

---
+++ ConsoleApp1/LinqToSql.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp1
+{
+    partial class LinqToSql
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLinqToSql = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLinqToSql
+            // 
+            this.txtLinqToSql.Location = new System.Drawing.Point(12, 12);
+            this.txtLinqToSql.Multiline = true;
+            this.txtLinqToSql.Name = "txtLinqToSql";
+            this.txtLinqToSql.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLinqToSql.Size = new System.Drawing.Size(315, 320);
+            this.txtLinqToSql.TabIndex = 0;
+            this.txtLinqToSql.TextChanged += new System.EventHandler(this.txtLinqToSql_TextChanged);
+            // 
+            // LinqToSql
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 347);
+            this.Controls.Add(this.txtLinqToSql);
+            this.Name = "LinqToSql";
+            this.Text = "LinqToSql";
+            this.Load += new System.EventHandler(this.LinqToSql_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLinqToSql;
+    }
+}
---
+++ ConsoleApp1/LinqToSql.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace ConsoleApp1
+{
+    public partial class LinqToSql : Form
+    {
+        public LinqToSql()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLinqToSql_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        [Table(Name = "Person.Person")]
+        public class Contact
+        {
+            [Column]
+            public string Title;
+            [Column]
+            public string FirstName;
+            [Column]
+            public string LastName;
+        }
+
+        private void LinqToSql_Load_1(object sender, EventArgs e)
+        {
+            // connection string
+
+            string connString = @"server = DESKTOP-V4UNJHJ;integrated security = true;database = AdventureWorks2017";
+            try
+            {
+                // Create data context
+                DataContext db = new DataContext(connString);
+                // Create typed table
+                Table<Contact> contacts = db.GetTable<Contact>();
+                // Query database
+                var contactDetails =
+                from c in contacts
+                where c.Title == "Mr."
+                orderby c.FirstName
+                select c;
+                // Display contact details
+                foreach (var c in contactDetails)
+                {
+                    txtLinqToSql.AppendText(c.Title);
+                    txtLinqToSql.AppendText("\t");
+                    txtLinqToSql.AppendText(c.FirstName);
+                    txtLinqToSql.AppendText("\t");
+                    txtLinqToSql.AppendText(c.LastName);
+                    txtLinqToSql.AppendText("\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
